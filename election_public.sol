3aceb6243d1a85642870494e0a864136e86b17df
EnockMuk <68714875+EnockMuk@users.noreply.github.com>
2021-04-21 16:27:38 -0700

    Update election_public.sol

--- election_public.sol
+++ election_public.sol
@@ -1,54 +1,74 @@
 pragma solidity ^0.7.0;
 
-contract Vote {
-        address admin;
-        uint public npmbredevoix;
-        uint public nombreDecandidat;
-        string public kure="Election troisieme cycle RDC KINSHASA";
-        uint candidatId;
-        
-        constructor(){
-            
-            admin= msg.sender;
-        }
+
+contract Election {
     
-        struct CANDIDAT {
-            
-            string name;
-            string lname;
-            uint Nvoix;
-            
-        }
-        struct VOTE {
-            
-            bool authorise;
-            bool deJaVote;
-            uint choixcandidat;
-                 
-        }
-     
-        CANDIDAT[]public candidat;
-        mapping(address=>VOTE)public electeurs;
+       string public nomelection=" Kinshasa Election edition 2022";
+       address admin;
+       uint public nombreDgensQuiontVote;
+       uint public nombreDecandidats;
+       constructor(){
+           
+           admin=msg.sender;
+         
+       }
+       
+       struct Candidat {
+           
+           string nom;
+           string pnom;
+           uint nombreDeVoix;
+           
+       }
+       
+       struct Processus {
+           
+           bool dejaVote;
+           bool autorise;
+           uint  choixDelecteur;
+           
+       }
+       
+       Candidat[]public candidats;
+       mapping(address=>Processus)public electeurs;
+       event AdressAutorise(address indexed administreur,address indexed autorisee);
+       event AjouterCand(address indexed administreur,string nomCandidat, string pnomCandidat, uint voix);
+       event VoteProcessus(address indexed electeur, uint choixFait);
+       modifier adminAuto(){
+           require(msg.sender==admin,"tu n'es pas l'admin");
+           _;
+       }
+       
+       
+       function ajouterCandidat(string memory _nom,string memory _pnom)public adminAuto {
+           
+           candidats.push(Candidat(_nom,_pnom,0));
+           candidats.push(Candidat(_nom,_pnom,0));
+           emit AjouterCand(msg.sender,_nom,_pnom,0);
+           nombreDecandidats++;
+           
+       }
+       
+       function autorisationDvote(address _add)public adminAuto{
+           
+           electeurs[_add].autorise=true;
+           emit AdressAutorise(msg.sender,_add);
+       }
+      
+       
+       function voter(uint _choixDelecteur)public {
+           require(electeurs[msg.sender].dejaVote==false,"tu as deja vote");
+           require(electeurs[msg.sender].autorise==true,"tu n'es autorise de voter");
+           electeurs[msg.sender].choixDelecteur=_choixDelecteur;
+           electeurs[msg.sender].dejaVote=true;
+           candidats[_choixDelecteur].nombreDeVoix++;
+           nombreDgensQuiontVote++;
+            emit VoteProcessus(msg.sender,_choixDelecteur);
+           
+       }
     
-        function AjouterCandidat(string memory _name, string memory _lname)public {
-            require(msg.sender==admin,"tu n'as pas le droit");
-            candidatId++;
-            candidat.push(CANDIDAT(_name,_lname,0));
-            nombreDecandidat++;
-        }
-        
-        function permission(address _add)public {
-            require(msg.sender==admin,"tu n'as pas ce droit");
-            electeurs[_add].authorise=true;
-        }
-        
-        function matora(uint _choixCandidat)public {
-            require(electeurs[msg.sender].deJaVote==false," you already vote, why do you want to cheat?");
-            require(electeurs[msg.sender].authorise==true,"you don't have the right to vote");
-            electeurs[msg.sender].choixcandidat=_choixCandidat;
-            electeurs[msg.sender].deJaVote=true;
-            candidat[_choixCandidat].Nvoix++;
-            npmbredevoix++;
-            
-        }   
+    
+    
+    
+    
 }
